3a0dd2fec581e89ff29bcd9177c129348902355d
Carlos Arias-Miranda <carlosar1219@hotmail.com>
2020-01-01 13:01:49 -0500

    Write auth methods for user model

--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -1,4 +1,42 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
+  validates :username, :session_token, :password_digest, presence: true
+  validates :username, :session_token, uniqueness: true
+  validates :password, length: { minimum: 6, allow_nil: true }
+
+  attr_reader :password
+
+  after_initialize :ensure_session_token
+
+  def self.find_by_credentials(username, password)
+    user = User.find_by(username: username)
+    user&.password?(password) ? user : nil
+  end
+
+  def password=(password)
+    @password = password
+    self.password_digest = BCrypt::Password.create(password)
+  end
+
+  def password?(password)
+    bcrypt_password = BCrypt::Password.new(password_digest)
+    bcrypt_password.password?(password)
+  end
+
+  def reset_session_token
+    self.session_token = User.generate_session_token
+    save
+    session_token
+  end
+
+  def ensure_session_token
+    self.session_token ||= User.generate_session_token
+  end
+
+  private
+
+  def self.generate_session_token
+    SecureRandom.urlsafe_base64
+  end
 end

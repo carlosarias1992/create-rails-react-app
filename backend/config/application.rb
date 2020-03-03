389c132b01cbd159e5f4b530120e0b18ce4596b1
Carlos Arias-Miranda <carlosar1219@hotmail.com>
2020-03-02 23:13:58 -0500

    Make react url dynamic

--- backend/config/application.rb
+++ backend/config/application.rb
@@ -25,7 +25,7 @@ module Entry
 
     config.middleware.insert_before 0, Rack::Cors do
       allow do
-        origins 'localhost:3000', '127.0.0.1:3000'
+        origins ENV['REACT_URL']
         resource '*', headers: :any, methods: %i[get post options delete], credentials: true
       end
     end
